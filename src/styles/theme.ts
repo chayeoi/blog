66e757cfde2004e75c496eeeea7a4ff24609b43f
chayeoi <chayeoikeem@gmail.com>
2020-02-11 02:01:07 +0900

    feat(media): breapoint를 설정하기 위해 `theme.breakpoints` 프로퍼티 추가
    `theme`에 breakpoints 값을 설정하고 미디어쿼리를 쉽게 사용하기 위한
    `media` API 추가
    
    Signed-off-by: chayeoi <chayeoikeem@gmail.com>

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,4 +1,34 @@
+import { css, SerializedStyles } from '@emotion/core'
+import _ from 'lodash/fp'
+
 import { hexToRgb, pxToRem } from '../utils'
+
+interface Sizes {
+  xs: number;
+  sm: number;
+  md: number;
+  lg: number;
+  xl: number;
+}
+
+interface Media {
+  xs?: (strings: string[], size: number, properties: SerializedStyles) => SerializedStyles;
+  sm?: (strings: string[], size: number, properties: SerializedStyles) => SerializedStyles;
+  md?: (strings: string[], size: number, properties: SerializedStyles) => SerializedStyles;
+  lg?: (strings: string[], size: number, properties: SerializedStyles) => SerializedStyles;
+  xl?: (strings: string[], size: number, properties: SerializedStyles) => SerializedStyles;
+}
+
+interface Breakpoints {
+  sizes: {
+    xs: number;
+    sm: number;
+    md: number;
+    lg: number;
+    xl: number;
+  };
+  media: Media;
+}
 
 interface Palette {
   primary: {
@@ -37,11 +67,31 @@ interface Typography {
 }
 
 export interface Theme {
+  breakpoints: Breakpoints;
   palette: Palette;
   typography: Typography;
 }
 
 const theme: Theme = {
+  breakpoints: {
+    sizes: {
+      xs: 0,
+      sm: 600,
+      md: 960,
+      lg: 1280,
+      xl: 1960,
+    },
+    get media() {
+      return _.reduce<string, Media>((acc, label) => ({
+        ...acc,
+        [label]: (...args: string[]) => css`
+          @media (min-width: ${this.sizes[label as keyof Sizes]}px) {
+            ${css(...args)}
+          }
+        `,
+      }), {}, _.keys(this.sizes))
+    },
+  },
   palette: {
     primary: {
       contrastText: '#ffffff',
