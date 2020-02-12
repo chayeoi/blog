9465ec9a934019f18092ff0c86b6c8ccd2304d1f
chayeoi <chayeoikeem@gmail.com>
2020-02-12 19:23:11 +0900

    style(palette): 변경된 eslint 규칙 적용
    연산자를 행의 시작 부분에 두도록 하는 eslint 규칙이 적용되지 않았던
    코드에 해당 규칙을 적용함
    
    Signed-off-by: chayeoi <chayeoikeem@gmail.com>

--- src/utils/palette.ts
+++ src/utils/palette.ts
@@ -4,9 +4,9 @@ export const hexToRgb = (hex: string, alpha?: number): string => {
   const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(longhandHex)
 
   if (result) {
-    return (alpha && alpha >= 0 && alpha <= 1) ?
-      `rgba(${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(result[3], 16)}, ${alpha})` :
-      `rgb(${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(result[3], 16)})`
+    return (alpha && alpha >= 0 && alpha <= 1)
+      ? `rgba(${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(result[3], 16)}, ${alpha})`
+      : `rgb(${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(result[3], 16)})`
   }
 
   return 'transparent'
