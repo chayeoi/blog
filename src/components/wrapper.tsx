ea2a7c80a55adc118a3ea6ad7c1a65ede4a7cf5e
chayeoi <chayeoikeem@gmail.com>
2020-02-21 21:16:29 +0900

    refactor: `wrap-root-element` api를 컴포넌트로 분리 (#15)
    `wrap-root-element`가 컴포넌트 타입이 아니기 때문에
    내부에서 hooks을 사용할 수 없는 문제를 해결하고자
    `wrap-root-element.ts`에서 담당하던 로직을 `wrapper.ts`로
    이동하고 `wrap-root-element.ts`에서는 `wrapper` 컴포넌트를
    사용하여 element를 렌더하도록 함
    
    Signed-off-by: chayeoi <chayeoikeem@gmail.com>

---
+++ src/components/wrapper.tsx
@@ -0,0 +1,42 @@
+import { Global } from '@emotion/core'
+import { MDXProvider } from '@mdx-js/react'
+import { ThemeProvider } from 'emotion-theming'
+import React from 'react'
+
+import global from '../styles/global'
+import theme from '../styles/theme'
+import * as MDXComponents from './mdx-components'
+
+interface Components {
+  [key: string]: React.FC;
+}
+
+const components: Components = {
+  a: MDXComponents.Anchor,
+  h1: MDXComponents.H1,
+  h2: MDXComponents.H2,
+  h3: MDXComponents.H3,
+  h4: MDXComponents.H4,
+  h5: MDXComponents.H5,
+  h6: MDXComponents.H6,
+  ul: MDXComponents.UnorderedList,
+  ol: MDXComponents.OrderedList,
+  li: MDXComponents.ListItem,
+  p: MDXComponents.Paragraph,
+  blockquote: MDXComponents.Blockquote,
+  table: MDXComponents.Table,
+  tr: MDXComponents.TableRow,
+  th: MDXComponents.TableCell,
+  td: MDXComponents.TableCell,
+}
+
+const Wrapper: React.FC = ({ children }) => (
+  <ThemeProvider theme={theme}>
+    <MDXProvider components={components}>
+      <Global styles={global} />
+      {children}
+    </MDXProvider>
+  </ThemeProvider>
+)
+
+export default Wrapper
