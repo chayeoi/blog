3d97d895465457609557a2fa868572c47159b980
chayeoi <chayeoikeem@gmail.com>
2020-02-25 19:55:28 +0900

    feat(splash): apple 디바이스에서 보여질 splash 스크린 설정 (#43)
    apple 디바이스에서 보여질 splash 스크린을
    디바이스 너비 및 해상도에 따라 설정함
    
    Signed-off-by: chayeoi <chayeoikeem@gmail.com>

---
+++ src/assets/images/index.ts
@@ -0,0 +1,23 @@
+import ipadSplash from './ipad_splash.png'
+import ipadpro1Splash from './ipadpro1_splash.png'
+import ipadpro2Splash from './ipadpro2_splash.png'
+import ipadpro3Splash from './ipadpro3_splash.png'
+import iphone5Splash from './iphone5_splash.png'
+import iphone6Splash from './iphone6_splash.png'
+import iphoneplusSplash from './iphoneplus_splash.png'
+import iphonexSplash from './iphonex_splash.png'
+import iphonexrSplash from './iphonexr_splash.png'
+import iphonexsmaxSplash from './iphonexsmax_splash.png'
+
+export const splash = {
+  ipad: ipadSplash,
+  ipadpro1: ipadpro1Splash,
+  ipadpro2: ipadpro2Splash,
+  ipadpro3: ipadpro3Splash,
+  iphone5: iphone5Splash,
+  iphone6: iphone6Splash,
+  iphoneplus: iphoneplusSplash,
+  iphonex: iphonexSplash,
+  iphonexr: iphonexrSplash,
+  iphonexsmax: iphonexsmaxSplash,
+}
