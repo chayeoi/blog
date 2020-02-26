16588220c98d43dbf403a7dcf5aadcd2320efef5
chayeoi <chayeoikeem@gmail.com>
2020-02-26 14:47:26 +0900

    feat: 포스트 목차를 추출하기 위한 유틸리티 함수 추가 (#17)
    포스트 목차를 원하는 형태의 포맷으로 추출하기 위한
    `getToc` 유틸리티를 추가
    
    - 목차의 각 섹션을 `slug`, `title`, `level`, `items` 필드를
      갖는 형태로 구조화
    - 하위 섹션을 최대 3단계 수준까지만 추출하도록 함
    - mdx 문서 내의 각 섹션의 하위 섹션 레벨이 한 단계 수준보다
      차이가 날 경우, 레벨을 한 단계 끌어올리도록 함
    - mdx 문서 내의 섹션 제목에 대하여 커스텀 링크가 존재하지 않을 경우,
      제목으로부터 링크를 자동 추출하도록 함
      - 공백 문자를 '-'로 대체하고 이모지와 특수문자를 제거하도록 함
    
    Signed-off-by: chayeoi <chayeoikeem@gmail.com>

---
+++ src/utils/regex.ts
@@ -0,0 +1,3 @@
+export const emoji = /<% emojiSequence %>|\p{Emoji_Presentation}|\p{Emoji}\uFE0F|\p{Emoji_Modifier_Base}/gu
+export const specials = /[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,./:;<=>?@[\]^`{|}~’]/g
+export const whitespace = /\s/g
