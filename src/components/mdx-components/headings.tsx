86b96c4fe3a2cc162c06766052f18fb79b9a04f5
chayeoi <chayeoikeem@gmail.com>
2020-02-27 01:17:17 +0900

    fix: 해시 링크 클릭 시 헤더에 의해 상단 제목이 가려지던 문제 해결 (#58)
    블로그 상세 화면에서 해시 링크 클릭 시 fixed header에 의해
    상단 제목이 가려지는 문제가 있었고, CSS의 `:target` 가상 선택자를
    이용하여 오프셋을 추가함으로써 문제를 해결함
    
    Signed-off-by: chayeoi <chayeoikeem@gmail.com>

--- src/components/mdx-components/headings.tsx
+++ src/components/mdx-components/headings.tsx
@@ -1,19 +1,30 @@
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core'
 
-export const H1: React.FC = props => <h1 css={s.h1} {...props} />
+import { HEADER_MIN_HEIGHT } from '../../constants'
 
-export const H2: React.FC = props => <h2 css={s.h2} {...props} />
+export const H1: React.FC = props => <h1 css={[s.offset, s.h1]} {...props} />
 
-export const H3: React.FC = props => <h3 css={s.h3} {...props} />
+export const H2: React.FC = props => <h2 css={[s.offset, s.h2]} {...props} />
 
-export const H4: React.FC = props => <h4 css={s.h4} {...props} />
+export const H3: React.FC = props => <h3 css={[s.offset, s.h3]} {...props} />
 
-export const H5: React.FC = props => <h5 css={s.h5} {...props} />
+export const H4: React.FC = props => <h4 css={[s.offset, s.h4]} {...props} />
 
-export const H6: React.FC = props => <h6 css={s.h6} {...props} />
+export const H5: React.FC = props => <h5 css={[s.offset, s.h5]} {...props} />
+
+export const H6: React.FC = props => <h6 css={[s.offset, s.h6]} {...props} />
 
 const s = {
+  offset: css`
+    :target:before{
+    content: '';
+    display: block;
+    height: ${HEADER_MIN_HEIGHT}px;
+    margin-top: -${HEADER_MIN_HEIGHT}px;
+    visibility: hidden;
+  }
+  `,
   h1: css`
     margin: 3rem 0 1.5rem;
     font-size: 1.625rem;
