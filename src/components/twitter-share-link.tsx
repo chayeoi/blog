88f56b7a079830d2d47442262093211924b2b15e
chayeoi <chayeoikeem@gmail.com>
2020-03-04 00:50:19 +0900

    refactor: 공유 링크에서 재사용되는 스타일을 개별 컴포넌트로 분리 (#90)
    공유 링크에서 반복적으로 사용되는 스타일을 `share-link`로 추출함
    
    Signed-off-by: chayeoi <chayeoikeem@gmail.com>

--- src/components/twitter-share-link.tsx
+++ src/components/twitter-share-link.tsx
@@ -1,13 +1,13 @@
 /** @jsx jsx */
-import { css, jsx, SerializedStyles } from '@emotion/core'
+import { css, jsx } from '@emotion/core'
 import { graphql, useStaticQuery } from 'gatsby'
 import _ from 'lodash/fp'
 import qs from 'query-string'
 import { useMemo } from 'react'
 
 import { twitterIcon } from '../assets/images'
-import { Theme } from '../models/Theme'
 import a11y from '../styles/a11y'
+import ShareLink from './share-link'
 
 interface Props {
   description: string;
@@ -39,7 +39,7 @@ const TwitterShareLink: React.FC<Props> = ({ description, tags, url, ...otherPro
   ]), [data.site.siteMetadata.twitter.name, description, tags, url])
 
   return (
-    <a
+    <ShareLink
       css={s.root}
       href={href}
       target="_blank"
@@ -48,29 +48,13 @@ const TwitterShareLink: React.FC<Props> = ({ description, tags, url, ...otherPro
       {...otherProps}
     >
       <span css={a11y}>트위터로 공유하기</span>
-    </a>
+    </ShareLink>
   )
 }
 
 const s = {
-  root: (theme: Theme): SerializedStyles => css`
-    display: block;
-    width: 36px;
-    height: 36px;
-    border-radius: 50%;
-    background: url(${twitterIcon}) center / cover no-repeat;
-    cursor: pointer;
-    outline: none;
-    transition: opacity 0.2s, box-shadow 0.2s, transform 0.2s;
-    :hover, :focus {
-      opacity: 0.85;
-      box-shadow: 0 4px 8px 0 ${theme.palette.hexToRgb(theme.palette.primary.dark, 0.1)};
-      transform: translateY(-4px);
-    }
-    ${theme.breakpoints.media.sm} {
-      width: 40px;
-      height: 40px;
-    }
+  root: css`
+    background-image: url(${twitterIcon});
   `,
 }
 
