f202c2000a6f33f9b653ebc7f80e7d5a4438f529
chayeoi <chayeoikeem@gmail.com>
2020-03-16 04:30:33 +0900

    feat: 포스트 상세 화면에서 링크 클릭 시 새 탭에서 열리도록 함 (#101)
    Signed-off-by: chayeoi <chayeoikeem@gmail.com>

--- src/components/mdx-components/anchor.tsx
+++ src/components/mdx-components/anchor.tsx
@@ -3,7 +3,7 @@ import { css, jsx, SerializedStyles } from '@emotion/core'
 
 import { Theme } from '../../models/Theme'
 
-export const Anchor: React.FC = props => <a css={s.a} {...props} />
+export const Anchor: React.FC = props => <a css={s.a} target="_blank" {...props} />
 
 const s = {
   a: (theme: Theme): SerializedStyles => css`
